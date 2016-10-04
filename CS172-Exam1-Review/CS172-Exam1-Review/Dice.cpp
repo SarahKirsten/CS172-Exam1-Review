f426c7f992d57cc075cb5aa2cdd7be36aa90df7b
SarahKirsten <deranged.link@gmail.com>
2016-10-04 13:43:34 -0700

    Basic Layout

---
+++ CS172-Exam1-Review/CS172-Exam1-Review/Dice.cpp
@@ -0,0 +1,15 @@
+/////////////////////////
+//Program Name: CS172-Exam1-Review
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 10/4/2016
+///////////////////////
+
+#include "Dice.h"
+using namespace std;
+
+int main()
+{
+
+
+}
---
+++ CS172-Exam1-Review/CS172-Exam1-Review/Dice.h
@@ -0,0 +1,19 @@
+/////////////////////////
+//Program Name: CS172-Exam1-Review
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 10/4/2016
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+
+#pragma once
